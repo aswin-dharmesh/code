da137919744388d6bf18fea160e028cc754bca10
Aswin Dharmesh <aswinstormshadow363@gmail.com>
2020-11-11 23:39:33 +0530

    multi class method

---
+++ circbox.java
@@ -0,0 +1,38 @@
+/*multiclass demo*/
+class circle{
+    double x,y,r;
+
+    double circumference(){
+        return 2*(22/7)*r;
+    }
+    double area(){
+        return (22/7)*r*r;
+    }
+}
+class box{
+    double l,b,h,a;
+
+    double areabox(){
+        a=l*b+b*h+h*l;
+        return a;
+    }
+}
+public class circbox {
+     public static void main(String[] args) {
+         circle c=new circle();
+         box b=new box();
+         c.r=5.5;
+         b.l=23.0;
+         b.b=21.0;
+         b.h=34.0;
+
+         System.out.println("circumference="+c.circumference());
+         System.out.println("area="+c.area());
+         System.out.println("Total Area"+b.areabox());
+         
+
+
+        
+    }
+    
+}
